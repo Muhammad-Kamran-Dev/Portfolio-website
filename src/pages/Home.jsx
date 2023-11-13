b2c0a51941f7bbd0510b1d4bc32f2f67158dca8d
Muhammad Kamran <MuhammadKamran.dev@gmail.com>
2023-11-13 10:58:04 +0500

    3d models added

---
+++ src/pages/Home.jsx
@@ -0,0 +1,84 @@
+import React, { Suspense, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Loader } from "../components";
+import Island from "../models/island";
+import Sky from "../models/sky";
+import Bird from "../models/Bird";
+import Plane from "../models/Plane";
+
+/* <div className="absolute top-28 inset-x-0 z-10 flex items-center justify-self-center">
+        POPUP
+      </div> */
+
+const Home = () => {
+  const [isRotating, setIsRotating] = useState(false);
+
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let isLandRotation = [0.1, 4.7, 0];
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+
+    return [screenScale, screenPosition, isLandRotation];
+  };
+  const adjustPlaneForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = null;
+
+    if (window.innerWidth < 768) {
+      screenScale = [1.5, 1.5, 1.5];
+      screenPosition = [0, -1.5, 0];
+    } else {
+      screenScale = [3, 3, 3];
+      screenPosition = [0, -4, -4];
+    }
+
+    return [screenScale, screenPosition];
+  };
+  const [isLandScale, islandPosition, isLandRotation] =
+    adjustIslandForScreenSize();
+
+  const [planeScale, planePosition] = adjustPlaneForScreenSize();
+  return (
+    <section className="w-full h-screen relative ">
+      <Canvas
+        className={`w-full h-screen bg-transparent ${
+          isRotating ? "cursor-grabbing" : "cursor-grab"
+        } `}
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight position={[1, 1, 1]} intensity={2} />
+          <ambientLight intensity={0.5} />
+          <hemisphereLight
+            skyColor={"#bee1ff"}
+            groundColor={"#000000"}
+            intensity={1}
+          />
+          <Bird />
+          <Sky />
+          <Island
+            position={islandPosition}
+            scale={isLandScale}
+            rotation={isLandRotation}
+            isRotating={isRotating}
+            setIsRotating={setIsRotating}
+          />
+          <Plane
+            position={planePosition}
+            scale={planeScale}
+            isRotating={isRotating}
+            rotation={[0, 20, 0]}
+          />
+        </Suspense>
+      </Canvas>
+    </section>
+  );
+};
+
+export default Home;
